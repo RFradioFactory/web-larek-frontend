f8202de8ef7f417644589c5f8584b5b3d1ab27ce
RFradioFactory <nevstruev.2004@mail.ru>
2025-04-11 20:35:06 +0500

    finish

--- src/components/View/Card.ts
+++ src/components/View/Card.ts
@@ -20,22 +20,42 @@ export class Card implements ICard {
   }
   
   constructor(template: HTMLTemplateElement, protected events: IEvents, actions?: IActions) {
+    this._cardElement = template.content.querySelector('.card').cloneNode(true) as HTMLElement;
+    this._cardCategory = this._cardElement.querySelector('.card__category');
+    this._cardTitle = this._cardElement.querySelector('.card__title');
+    this._cardImage = this._cardElement.querySelector('.card__image');
+    this._cardPrice = this._cardElement.querySelector('.card__price');
     
+    if (actions?.onClick) {
+      this._cardElement.addEventListener('click', actions.onClick);
+    }
   }
 
   protected setText(element: HTMLElement, value: unknown): string {
-    
+    if (element) {
+      return element.textContent = String(value);
+    }
   }
 
   set cardCategory(value: string) {
-    
+    this.setText(this._cardCategory, value);
+    this._cardCategory.className = `card__category card__category_${this._colors[value]}`
   }
 
   protected setPrice(value: number | null): string {
-    
+    if (value === null) {
+      return 'Бесценно'
+    }
+    return String(value) + ' синапсов'
   }
 
   render(data: IProductItem): HTMLElement {
-    
+    this._cardCategory.textContent = data.category;
+    this.cardCategory = data.category;
+    this._cardTitle.textContent = data.title;
+    this._cardImage.src = data.image;
+    this._cardImage.alt = this._cardTitle.textContent;
+    this._cardPrice.textContent = this.setPrice(data.price);
+    return this._cardElement;
   }
 }
--- src/components/View/Success.ts
+++ src/components/View/Success.ts
@@ -13,10 +13,15 @@ export class Success {
   button: HTMLButtonElement;
 
   constructor(template: HTMLTemplateElement, protected events: IEvents) {
-    
+    this.success = template.content.querySelector('.order-success').cloneNode(true) as HTMLElement;
+    this.description = this.success.querySelector('.order-success__description');
+    this.button = this.success.querySelector('.order-success__close');
+
+    this.button.addEventListener('click', () => { events.emit('success:close') });
   }
 
   render(total: number) {
-    
+    this.description.textContent = String(`Списано ${total} синапсов`);
+    return this.success
   }
 }
